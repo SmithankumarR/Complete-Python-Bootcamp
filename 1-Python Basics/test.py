f129613bccd7d52e6a8995af2896989ea5390da5
SmithankumarR <smithankumar404@gmail.com>
2026-04-04 12:42:00 +0530

    Learn't and practiced basics of python

--- 1-Python Basics/test.py
+++ 1-Python Basics/test.py
@@ -3,8 +3,11 @@
 this is a exmaple of multiline comments
 '''
 '''
-Welcome to the
-python course
+Finally complete the basicis of python, now we can move on to the next
+ section which is data types in python  
+   
+finished the datatypes section, now we can move on to the next 
+ section which is basic operators in python
 
 
 '''
