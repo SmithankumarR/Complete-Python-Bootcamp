1ddfd6870c9ba5eb5d33b2dd99c90b1e8155085a
SmithankumarR <smithankumar404@gmail.com>
2026-08-31 09:35:35 +0530

    created a basic components and app using streamlit

--- 14-Streamlit/app.py
+++ 14-Streamlit/app.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 
 ## Title of the aplication
-st.title("Hello Streamlit")
+st.title("Hello Streamlit, i am developing a simple web application using streamlit")
 
 ## Diplay a Simple Text
-st.write("This is a imple text")
+st.write("This is my first web application using streamlit")
 
-##create a simple Dataframe
+# ##create a simple Dataframe
 
 df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
@@ -17,13 +17,13 @@ df = pd.DataFrame({
 
 
 ## Display the Dataframe
-st.write("Here is the dataframe")
+st.write("Here is the dataframe created using pandas library")
 st.write(df)
 
 
-##create a line chart
+# ##create a line chart
 
 chart_data=pd.DataFrame(
-    np.random.randn(20,3),columns=['a','b','c']
+    np.random.randn(20,3),columns=['name','party','category']
 )
 st.line_chart(chart_data)
--- 14-Streamlit/widgets.py
+++ 14-Streamlit/widgets.py
@@ -11,7 +11,7 @@ age=st.slider("Select your age:",0,100,25)
 st.write(f"Your age is {age}.")
 
 options = ["Python", "Java", "C++", "JavaScript"]
-choice = st.selectbox("Choose your favorite language:", options)
+choice = st.selectbox("Choose your favorite language:", options) # to create dropdown
 st.write(f"You selected {choice}.")
 
 if name:
@@ -19,9 +19,9 @@ if name:
 
 
 data = {
-    "Name": ["John", "Jane", "Jake", "Jill"],
+    "Name": ["John smith", "Jane", "Jamilere", "kelly"],
     "Age": [28, 24, 35, 40],
-    "City": ["New York", "Los Angeles", "Chicago", "Houston"]
+    "City": ["New York", "Los Angeles", "Chicago", "bangalore"]
 }
 
 df = pd.DataFrame(data)
@@ -29,7 +29,7 @@ df.to_csv("sampledata.csv")
 st.write(df)
 
 
-uploaded_file=st.file_uploader("Choose a CSV file",type="csv")
+uploaded_file=st.file_uploader("Choose a CSV file",type="csv") # to up-load a file
 
 if uploaded_file is not None:
     df=pd.read_csv(uploaded_file)
